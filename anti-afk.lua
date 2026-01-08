eb86661810dcf01688facbdd33927683f2567385
NN-EZ <86054483+NN-EZ@users.noreply.github.com>
2026-01-08 19:10:59 +0500

    anti-afk.lua

---
+++ anti-afk.lua
@@ -0,0 +1,20 @@
+local Players = game:GetService("Players")
+local VirtualUser = game:GetService("VirtualUser")
+local LocalPlayer = Players.LocalPlayer
+
+if getconnections then
+    for _, connection in pairs(getconnections(LocalPlayer.Idled)) do
+        if connection["Disable"] then
+            connection["Disable"](connection)
+        elseif connection["Disconnect"] then
+            connection["Disconnect"](connection)
+        end
+    end
+else
+    LocalPlayer.Idled:Connect(function()
+        VirtualUser:CaptureController()
+        VirtualUser:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+        wait(1)
+        VirtualUser:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+    end)
+end
